d42139e1130255992c1e7b5edc9348e649504fea
christopher kasoma <chriscastle@Bogere-Bali-s-MacBook-Air.local>
2017-11-17 13:52:56 +0900

    change of title

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Storm Viewer"
+        title = "Action"
             // Do any additional setup after loading the view, typically from a nib.
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
